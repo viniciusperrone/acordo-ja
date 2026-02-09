cc2a3f663f29f597526d7bc9afb4423886f23491
Vinicius Perrone <perronevinicius2018@gmail.com>
2026-02-08 21:51:22 -0300

    feat: created connection models

--- backend/app.py
+++ backend/app.py
@@ -6,16 +6,17 @@ from config.config import Config
 
 import debts
 
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = Config.SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = Config.SQLALCHEMY_TRACK_MODIFICATIONS
+
+db.init_app(app)
+
+migrate = Migrate(app, db)
+
 
 if __name__ == '__main__':
-    app = Flask(__name__)
-
-    app.config["SQLALCHEMY_DATABASE_URI"] = Config.SQLALCHEMY_DATABASE_URI
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = Config.SQLALCHEMY_TRACK_MODIFICATIONS
-
-    db.init_app(app)
-
-    migrate = Migrate(app, db)
 
     app.run(
         host='0.0.0.0',
--- backend/config/config.py
+++ backend/config/config.py
@@ -1,5 +1,5 @@
 
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://user:password@db:5432/acordoj"
+    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://postgres:postgres@db:5432/acordoja"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
