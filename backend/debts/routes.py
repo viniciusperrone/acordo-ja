5a2f26105c468b81488faac96b26d57530d5f48a
Vinicius Perrone <perronevinicius2018@gmail.com>
2026-02-11 15:36:48 -0300

    feat: added new fields created_at and updated_at

--- backend/debts/routes.py
+++ backend/debts/routes.py
@@ -2,15 +2,28 @@ from flask import Blueprint, request, jsonify
 
 from config.db import db
 from debts import Debt
+from debts.schemas import DebtSchema
 
 debts_bp = Blueprint('debts', __name__, url_prefix='/debts')
 
 @debts_bp.route('/add', methods=['POST'])
-def create():
-    data= request.json
+def create_debt():
+    data = request.json
+    debt_schema = DebtSchema()
+
+    errors = debt_schema.validate(data)
+
+    if errors:
+        return jsonify(errors), 400
+
+
     debt = Debt(**data)
 
-    db.session.add(debt)
-    db.session.commit()
+    try:
+        db.session.add(debt)
+        db.session.commit()
 
-    return jsonify({'message': 'Dívida cadastrada com sucesso'})
+        return jsonify({'message': 'Successfully registered debt'})
+    except Exception:
+
+        return jsonify({"message": "Internal Server Error"}), 500
