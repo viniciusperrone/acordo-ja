9752e05d36999ee214f55319f2103499908233fc
Vinicius Perrone <perronevinicius2018@gmail.com>
2026-02-27 23:36:34 -0300

    refactor: updated PaymentSchema

--- backend/payment/schemas.py
+++ backend/payment/schemas.py
@@ -3,14 +3,14 @@ from utils.enum import MethodPayment
 
 
 class PaymentSchema(Schema):
+    id = fields.UUID(dump_only=True)
     amount = fields.Decimal(required=True, as_string=True)
-
     method = fields.Enum(
         MethodPayment,
         by_value=True,
         required=True
     )
-
+    installment_id = fields.Integer(dump_only=True)
     paid_at = fields.DateTime(required=False)
 
     class Meta:
