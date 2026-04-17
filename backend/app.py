3d89e98acbd78de6afe19b424b0bbb9ec59a094d
Vinicius Perrone <perronevinicius2018@gmail.com>
2026-04-17 16:54:01 -0300

    chore: add Redis service

--- backend/app.py
+++ backend/app.py
@@ -5,8 +5,6 @@ from logging.handlers import RotatingFileHandler
 from flask import Flask
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
-from flask_limiter import Limiter
-from flask_limiter.util import get_remote_address
 
 from config.db import db
 from config.config import Config
