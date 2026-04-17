814e69aefcae1a8becdd26ba6044eaa077a0ffd1
Vinicius Perrone <perronevinicius2018@gmail.com>
2026-04-17 17:02:26 -0300

    refactor: updated Limiter instance

--- backend/config/rate_limit.py
+++ backend/config/rate_limit.py
@@ -2,4 +2,8 @@ from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 
 
-limiter = Limiter(key_func=get_remote_address)
+limiter = Limiter(
+    key_func=get_remote_address,
+    storage_uri="redis://redis:6379",
+    default_limits=["100 per hour"]
+)
