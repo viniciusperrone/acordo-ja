350cfc9cd0f630f9b15289243da621f687b4b6ce
Vinicius Perrone <perronevinicius2018@gmail.com>
2026-04-28 22:12:13 -0300

    test: add main fixtures

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,38 +1,56 @@
-import os
 import pytest
-
-from app import initialize_app
-from config.db import db
-
+from datetime import datetime, date
+from decimal import Decimal
+import os
 
 os.environ["TESTING"] = "True"
 
-@pytest.fixture()
+from app import initialize_app
+from config.db import db as _db
+from users.models import User
+from debtor.models import Debtor
+from creditor.models import Creditor
+from debts.models import Debt
+
+
+@pytest.fixture(scope="session")
 def app():
     app = initialize_app()
-
     app.config.update({
         "TESTING": True,
-        "SQLALCHEMY_DATABASE_URI": "sqlite:///test.db",
-        "SQLALCHEMY_TRACK_MODIFICATIONS": True
+        "SQLALCHEMY_DATABASE_URI":  "sqlite:///:memory:",
+        "SQLALCHEMY_TRACK_MODIFICATIONS": False,
+        "JWT_SECRET_KEY": os.getenv("JWT_SECRET_KEY")
     })
-
-    with app.app_context():
-        db.create_all()
 
     yield app
 
-@pytest.fixture()
-def client(app):
-    return app.test_client()
+@pytest.fixture(scope="session")
+def db(app):
 
-@pytest.fixture()
-def runner(app):
-    return app.test_cli_runner()
+    with app.app_context():
+        _db.create_all()
+        yield _db
+        _db.drop_all()
+
+@pytest.fixture(scope="session")
+def session(db, app):
+
+    with app.app_context():
+        connection = db.engine.connect()
+        transaction = connection.begin()
+
+        session = db.create_scoped_session(
+            options={"bind": connection, "binds": {}}
+        )
+        db.session = session
+
+        yield session
+
+        session.close()
+        transaction.rollback()
+        connection.close()
 
 @pytest.fixture(scope="function")
-def db_session(app):
-    with app.app_context():
-        yield db.session
-        db.session.rollback()
-        db.session.remove()
+def client(app):
+    return app.test_client()
