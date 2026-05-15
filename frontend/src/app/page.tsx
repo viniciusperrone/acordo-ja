ea99d4d003d41dc45b934cecad6347229911d4e1
Vinicius Perrone <perronevinicius2018@gmail.com>
2026-05-15 11:29:56 -0300

    feat: complete home page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import Header from "../components/header";
 import Footer from "../components/footer";
 import illustration from "../assets/images/Ilustration.png";
+import qrCode from "../assets/images/QR_Code.png";
 import moneyIcon from "../assets/money-icon.png";
 
 const floatingIcons = [
@@ -43,7 +44,7 @@ export default function Home() {
           </div>
         ))}
 
-        <div className="relative z-20 min-w-full min-h-full flex items-center ">
+        <div className="relative z-20 min-w-full min-h-full flex flex-row items-center justify-between pr-[8%]">
           <div className="pl-[22%]">
             <Image
               src={illustration}
@@ -52,6 +53,28 @@ export default function Home() {
               width={195}
             />
           </div>
+          <div className="w-100 py-4 px-6 flex flex-col gap-6 rounded-3xl bg-[#76B74F]">
+              <h3 className="text-2xl font-bold self-center">Consulte sua dívida agora!</h3>
+              <div className="flex flex-col gap-1">
+                <label className="text-inter text-[14px] font-semibold" htmlFor="">Documento</label>
+                <input type="text" className="h-10 border border-[#404040] bg-white text-[#6C757D] rounded-sm px-1.5" placeholder="Informe seu CPF ou CNPJ"/>
+              </div>
+              <button className="h-10 bg-[#1F6C74] font-inter font-bold text-[14px] text-white rounded-lg">Negociar dívida</button>
+              <div className="flex flex-row items-center justify-center gap-2">
+                <div className="h-px bg-[#07513B] w-30"/>
+                <p>ou</p>
+                <div className="h-px bg-[#07513B] w-30"/>
+              </div>
+              <p className="text-[14px] text-center">acesse com o QR Code</p>
+              <div className="self-center">
+                <Image 
+                  src={qrCode}
+                  alt="QR Code"
+                  width={80}
+                  height={80}
+                />
+              </div>
+            </div>
         </div>
       </main>
       <Footer />
