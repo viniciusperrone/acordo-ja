e4336aee71d46deca65ee0b49623b6dc60b8aa84
Vinicius Perrone <perronevinicius2018@gmail.com>
2026-06-01 16:27:37 -0300

    style: fix lint violations according to flake8 standards in app.py

--- backend/app.py
+++ backend/app.py
@@ -16,16 +16,16 @@ from config import (
     init_jwt,
 )
 
-import creditor # noqa: F401
-import debts # noqa: F401
-import debtor # noqa: F401
-import agreement # noqa: F401
-import installments # noqa: F401
-import payment # noqa: F401
-import users # noqa: F401
-import notifications # noqa: F401
-import leads # noqa: F401
-import authentication # noqa: F401
+import creditor
+import debts
+import debtor
+import agreement
+import installments
+import payment
+import users
+import notifications
+import leads
+import authentication
 
 from creditor.routes import creditor_bp
 from debts.routes import debts_bp
@@ -80,6 +80,7 @@ def initialize_app():
         init_scheduler(app)
 
     return app
+
 
 if __name__ == '__main__':
     app = initialize_app()
